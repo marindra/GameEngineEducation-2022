a3a35d1a9ed75ca8afe130ef4716594d785996f2
marindra <marindra@yandex.ru>
2023-09-22 01:04:10 +0300

    Added motion along X axis and config files.

--- Lesson2/SourceCode/GameEngine/GameEngine.cpp
+++ Lesson2/SourceCode/GameEngine/GameEngine.cpp
@@ -1,6 +1,6 @@
 // GameEngine.cpp : Defines the entry point for the application.
 //
-
+#include <iostream>
 #if defined(DEBUG) || defined(_DEBUG)
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
@@ -12,6 +12,23 @@
 #include "CubeGameObject.h"
 #include "GameTimer.h"
 
+#include "Additional files/INIReader.h"
+// Added _CRT_SECURE_NO_WARNINGS due to unsafe functions in ini.h
+
+unsigned int GetCode(const std::string& line, unsigned int defaultValue) {
+    if (line.length() != 1) {
+        return defaultValue;
+    }
+
+    char curSymb = line[0];
+    if (curSymb >= 'a' && curSymb <= 'z') {
+        return toupper(curSymb);
+    }
+    if (curSymb >= 'A' && curSymb <= 'Z') {
+        return curSymb;
+    }
+    return defaultValue;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -23,6 +40,19 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
+    INIReader reader("Config/actionmap.ini");
+    if (reader.ParseError() < 0) {
+        std::cout << "Can't load 'actionmap.ini'\n";
+        std::cout << "Path to config: 'Config/actionmap.ini'\n";
+        return 1;
+    }
+
+    const std::string goLeft = reader.Get("Keyboard", "GoLeft", "A");
+    unsigned int codeLeft = GetCode(goLeft, (unsigned int)'A');
+
+    const std::string goRight = reader.Get("Keyboard", "GoRight", "D");
+    unsigned int codeRight = GetCode(goRight, (unsigned int)'D');
+
     GameTimer timer;
 
     RenderEngine* renderEngine = new RenderEngine(hInstance);
@@ -32,6 +62,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     renderThread->EnqueueCommand(RC_CreateCubeRenderObject, cube->GetRenderProxy());
 
     MSG msg = { 0 };
+
+//    bool needToSet = false;
+    float cubeXTranslate = 0.f;
+    const float maxSpeedX = 2.f;
+    float speedX = 0.f;
 
     timer.Start();
     timer.Reset();
@@ -43,13 +78,29 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+
+            switch (msg.message)
+            {
+            case WM_KEYDOWN:
+                if (GetAsyncKeyState(codeLeft) & 0x01)
+                {
+                    speedX = -maxSpeedX;
+                }
+                else if (GetAsyncKeyState(codeRight) & 0x01) {
+                    speedX = maxSpeedX;
+                }
+                break;
+            }
         }
         else
         {
-            float t = 0;
+            //float t = 0;
             timer.Tick();
-            t = sin(timer.TotalTime())*2;
-            cube->SetPosition(t, 0.0f, 0.0f);
+            //t = sin(timer.TotalTime())*2;
+            cubeXTranslate += speedX * timer.DeltaTime();
+            speedX = 0;
+
+            cube->SetPosition(cubeXTranslate, 0.0f, 1.0f);
 
             renderThread->OnEndFrame();
         }
